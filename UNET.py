eb0d4703a7c6d938e7105c8de6663e814e8437c5
Markus Ellingsgaard <s224384@dtu.dk>
2024-11-11 16:33:43 +0100

    Missing rest of forward()

---
+++ UNET.py
@@ -0,0 +1,60 @@
+import torch
+import numpy as np
+import torch.nn as nn
+
+class UNET(torch.nn.Module):
+    def __init__(self):
+        super(UNET, self).__init__()
+        channels = [32, 64, 128, 256]
+        self._convs = nn.ModuleList([
+            nn.Sequential(
+                nn.Conv2d(2, channels[0], kernel_size=3, padding=1),  # (batchsize, 32, 28, 28)
+                nn.ReLU()
+            ),
+            nn.Sequential(
+                nn.MaxPool2d(2),  # (batchsize, 32, 14, 14)
+                nn.Conv2d(channels[0], channels[1], kernel_size=3, padding=1),  # (batchsize, 64, 14, 14)
+                nn.ReLU()
+            ),
+            nn.Sequential(
+                nn.MaxPool2d(2),  # (batchsize, 64, 7, 7)
+                nn.Conv2d(channels[1], channels[2], kernel_size=3, padding=1),  # (batchsize, 128, 7, 7)
+                nn.ReLU()
+            ),
+            nn.Sequential(
+                nn.MaxPool2d(2, padding=1),  # (batchsize, 128, 4, 4)
+                nn.Conv2d(channels[2], channels[3], kernel_size=3, padding=1),  # (batchsize, 256, 4, 4)
+                nn.ReLU()
+            )
+        ])
+
+        self._tconvs = nn.ModuleList([
+            nn.Sequential(
+                nn.ConvTranspose2d(channels[3], channels[2], kernel_size=3, 
+                                   stride=2, padding=1, output_padding=0),   # (batchsize, 128, 7, 7)
+                nn.ReLU()
+            ),
+            nn.Sequential(
+                nn.ConvTranspose2d(channels[2]*2, channels[1], kernel_size=3,
+                                   stride=2, padding=1, output_padding=1),   # (batchsize, 64, 14, 14)
+                nn.ReLU()
+            ),
+            nn.Sequential(
+                nn.ConvTranspose2d(channels[1]*2, channels[0], kernel_size=3, 
+                                   stride=2, padding=1, output_padding=1),   # (batchsize, 32, 28, 28)
+                nn.ReLU()
+            ),
+            nn.Sequential(
+                nn.conv2d(channels[0]*2,channels[0],kernel_size=3,padding=1),  # (batchsize, 32, 28, 28)
+                nn.ReLU(),
+                nn.Conv2d(channels[0],1,kernel_size=1) # (batchsize, 1, 28, 28)
+            )      
+        ])
+
+    def forward(self, x, t):
+        x_trans = torch.cat((x, t), dim=-3)
+        signal = x_trans
+        signals = []
+
+        for i, conv in enumerate(self._convs):
+            
